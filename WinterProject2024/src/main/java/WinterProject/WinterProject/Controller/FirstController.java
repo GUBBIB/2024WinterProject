7aa26acbd3461f3303e0f6ece385ea5cd698a31c
GUBBIB <moon.dragon250@gmail.com>
2024-12-30 04:24:31 +0900

    Update

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Controller/FirstController.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Controller/FirstController.java
@@ -1,12 +1,21 @@
 package WinterProject.WinterProject.Controller;
 
+import WinterProject.WinterProject.Entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/register")
     public String goToRegister(){
         return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String save(User user){
+        System.out.println(user.toString());
+
+        return "";
     }
 }
---
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package WinterProject.WinterProject.Repository;
+
+import WinterProject.WinterProject.Entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
