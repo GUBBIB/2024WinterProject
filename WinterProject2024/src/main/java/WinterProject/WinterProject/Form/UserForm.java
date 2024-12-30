e90af81014d4d671fa595b0dec4bb16de7fd0300
GUBBIB <moon.dragon250@gmail.com>
2024-12-31 00:55:24 +0900

    Update UserForm.java

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Form/UserForm.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Form/UserForm.java
@@ -3,8 +3,7 @@ package WinterProject.WinterProject.Form;
 import WinterProject.WinterProject.Entity.User;
 
 public class UserForm {
-    private Long user_id;
-    private String email, password, name;
+    private String user_id, email, password, name;
 
     public UserForm(Long user_id, String email, String password, String name) {
         this.user_id = user_id; this.email = email;
