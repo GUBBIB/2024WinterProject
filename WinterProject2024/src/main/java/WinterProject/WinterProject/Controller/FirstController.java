6b2e7ea98f55bc039e47bde0dd5f8ecc2731d2d9
GUBBIB <moon.dragon250@gmail.com>
2024-12-30 21:37:40 +0900

    Update FirstController.java

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Controller/FirstController.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Controller/FirstController.java
@@ -2,7 +2,7 @@ package WinterProject.WinterProject.Controller;
 
 import WinterProject.WinterProject.Entity.User;
 import WinterProject.WinterProject.Service.UserService;
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
