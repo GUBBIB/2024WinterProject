d5539177fbbcd9ad00df77169c19f4a2cbedefd8
GUBBIB <moon.dragon250@gmail.com>
2024-12-31 22:45:27 +0900

    Udpate

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Controller/UserController.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Controller/UserController.java
@@ -3,6 +3,7 @@ package WinterProject.WinterProject.Controller;
 import WinterProject.WinterProject.Entity.User;
 import WinterProject.WinterProject.Form.UserForm;
 import WinterProject.WinterProject.Repository.UserRepository;
+import WinterProject.WinterProject.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,13 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController{
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @PostMapping("/register/save")
-    public String save(UserForm form, Model model){
+    public String save(User user, Model model){
         try {
-            User user = form.toEntity();
-            User saved = userRepository.save(user);
+            userService.saveUser(user);
         } catch(Exception e){
             model.addAttribute("error", e.toString());
             return "redirect:/error";
