5388f8f5c0bd8e5bf16d7a384b7eb021b2f7e6c3
GUBBIB <moon.dragon250@gmail.com>
2025-01-02 04:17:49 +0900

    Update UserRepository.java

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Repository/UserRepository.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Repository/UserRepository.java
@@ -4,7 +4,10 @@ import WinterProject.WinterProject.Entity.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
     Optional<User> findByUserId(String user_id);
+
 }
