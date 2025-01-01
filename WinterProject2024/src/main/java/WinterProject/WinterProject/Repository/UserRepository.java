498811626465c39ac5eaca786fc5d0cb55515423
GUBBIB <moon.dragon250@gmail.com>
2025-01-02 04:14:52 +0900

    Update

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Repository/UserRepository.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
-
+    Optional<User> findByUserId(String user_id);
 }
