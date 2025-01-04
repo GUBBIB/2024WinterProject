6f8a9724560959c7134f73e3e301ac84dced692a
GUBBIB <moon.dragon250@gmail.com>
2025-01-05 04:16:26 +0900

    Update SecurityConfig.java

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Config/SecurityConfig.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Config/SecurityConfig.java
@@ -1,26 +1,26 @@
-package WinterProject.WinterProject.Config;
+// package WinterProject.WinterProject.Config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf().disable()
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/login", "/register", "/error", "/chk", "/register/saveUser").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                );
+//         http
+//                 .csrf().disable()
+//                 .authorizeHttpRequests((auth) -> auth
+//                         .requestMatchers("/").permitAll()
+//                         .requestMatchers("/login", "/register", "/error", "/chk", "/register/saveUser").permitAll()
+//                         .requestMatchers("/admin").hasRole("ADMIN")
+//                 );
 
-        return http.build();
-    }
-}
+//         return http.build();
+//     }
+// }
