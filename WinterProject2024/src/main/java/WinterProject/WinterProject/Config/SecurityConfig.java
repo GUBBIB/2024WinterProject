701ddfe2d89a68f3cb7d036605e891f7b8d77841
GUBBIB <moon.dragon250@gmail.com>
2025-01-05 04:07:04 +0900

    Update SecurityConfig.java

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Config/SecurityConfig.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .csrf().disable()
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/login", "/register").permitAll()
