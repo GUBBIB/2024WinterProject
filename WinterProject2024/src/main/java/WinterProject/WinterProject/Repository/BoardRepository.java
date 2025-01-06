685fe9aaf14b43142e293aec3e52cc02a5559896
GUBBIB <moon.dragon250@gmail.com>
2025-01-06 22:01:17 +0900

    Update

--- WinterProject2024/src/main/java/WinterProject/WinterProject/Repository/BoardRepository.java
+++ WinterProject2024/src/main/java/WinterProject/WinterProject/Repository/BoardRepository.java
@@ -1,9 +1,10 @@
 package WinterProject.WinterProject.Repository;
 
-import WinterProject.WinterProject.Entity.Post;
+import WinterProject.WinterProject.Entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
 }
